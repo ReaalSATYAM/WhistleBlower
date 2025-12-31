32811c6652f212494835b30775a847698111fed1
Satyam Naithani <149092305+ReaalSATYAM@users.noreply.github.com>
2025-12-31 19:01:38 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import Home from "./pages/Home";
 import Status from "./pages/Status";
 import AdminLogin from "./components/AdminLogin";
-import AdminDashboard from "./components/AdminDashboard"; // <--- NEW IMPORT
+import AdminDashboard from "./components/AdminDashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/WhistleBlower">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/status" element={<Status />} />
-        
+
         {/* Admin Routes */}
         <Route path="/admin" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} /> {/* <--- NEW ROUTE */}
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   );
